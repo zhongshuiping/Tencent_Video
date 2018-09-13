e521a212b26137ec87954988a133c31f967fc541
dk <zeqizai@163.com>
2018-09-13 09:52:24 +0800

    VideoListSpider自动化

--- start_VideoListSpider.py
+++ start_VideoListSpider.py
@@ -7,10 +7,9 @@ def job():
 
 def cron_job():
     sched = BlockingScheduler()
-    # sched.add_job(job, 'cron', hour='16', minute='30', day_of_week='tue,thu,sat', id='iqiyi_video')
-    sched.add_job(job, 'cron', day_of_week='sat', hour='5', id='iqiyi_video')
+    sched.add_job(job, 'cron', hour='10', id='VideoListSpider')
     sched.start()
 
 if __name__ == '__main__':
-    job()
+    cron_job()
 
