3fe81bf366bdb935797968441bf1bd30a58bfaee
dk <zeqizai@163.com>
2018-10-09 18:17:25 +0800

    测试最大用户id

--- TaskPut_UserInfo.py
+++ TaskPut_UserInfo.py
@@ -8,17 +8,26 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 r = RedisHelper().client
 user_redis_key = 'TX_Video_UserInfoSpider_key'
+combo_flag = True
+COMBO_TOPID = ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
 
-def get_sample():
-    uid_list = random.sample(range(0, 10000 * 10000 * 10), 100 * 10000) # 全部随机抽取50w
+def get_sample(combo_flag):
+    uid_list = []
+    if not combo_flag:
+        uid_list = random.sample(range(0, 10000 * 10000 * 10), 100 * 10000) # 全部随机抽取50w
+    else:
+        for topid in COMBO_TOPID:
+            uid_list.extend(random.sample(range(int(topid) * 10000 * 10000,
+                                                (int(topid)+1) * 10000 * 10000),
+                                          5 * 10000))
     return uid_list
 
-def generate_uid():
-    uid_list = get_sample()
+def generate_uid(combo_flag):
+    uid_list = get_sample(combo_flag)
     ThreadPoolExecutor(30).map(input_onetask, uid_list)
 
 def job():
-    generate_uid()
+    generate_uid(combo_flag)
     os.system("scrapy crawl UserInfoSpider")
 
 def input_onetask(uid):
@@ -30,4 +39,4 @@ def cron_job():
     sched.start()
 
 if __name__ == '__main__':
-    generate_uid()
+    generate_uid(combo_flag)
